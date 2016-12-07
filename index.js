df622e1c47a6af004dbf936d713979e15b057478
David García <daangar@gmail.com>
2016-12-06 22:30:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ app.get('/', function(req, res) {
   res.status(200).send('Este es mi servidor.');
 });
 
+//Verificacion para boot
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === <this_is_my_token>) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 var port = process.env.PORT || 1337;
 app.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
